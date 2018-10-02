b8c4778bf9cf9a936f840baebad05ced447feccf
Pooya Daravi <daravi@alumni.ubc.ca>
2018-10-01 21:10:59 -0700

    Add instructions to README.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-...
+Please install miniconda from https://conda.io/miniconda.html
+
+Then to setup the necessary environment run:
+
+```>> conda env create -f py3.yml```
+
+You can then do:
+
+```>> conda activate py3```
+
+```>> python data-grabber.py```
+
+The list of accounts and vendors will be written to json files in the working directory.
--- data-grabber.py
+++ data-grabber.py
@@ -1,19 +1,20 @@
 from xero import Xero
-from xero.auth import PrivateCredentials
 from xero.auth import PublicCredentials
 import webbrowser
 import json
 
-# BeanworksTest-Public
+# Credentials for BeanworksTest-Public
 credentials = PublicCredentials(
     "0L67EMJGHO5TPNV9ECR7KLZ4RY2GLY", "3ZPJSGGFKHXULYXTRVJE8OYUCORX4B")
+# Validate session
 webbrowser.open(credentials.url)
 verification_code = input("Please enter the verification code: ")
 credentials.verify(verification_code)
 xero = Xero(credentials)
+# GET list of accounts and vendors
 accounts = xero.accounts.all()
 vendors = xero.contacts.filter(IsSupplier=True)
-# print(vendors, accounts)
+# Write accounts and vendors to disk
 with open('accounts.json', 'w') as outfile:
     json.dump(accounts, outfile, indent=2, default=str)
 with open('vendors.json', 'w') as outfile:
